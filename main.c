4b6d0ffdeb52d284feff8cccccb17a52e9e7a863
asahi-nixos <Lawrence.Matsuoka@proton.me>
2025-03-17 10:17:58 -0300

    rewrote the configureADC and readADC to initADC()

--- main.c
+++ main.c
@@ -8,53 +8,42 @@
 
 double noteFrequencies[6] = {82.0, 110.0, 147.0, 196.0, 247.0, 330.0};
 
-//void configureADC(void);
-//unsigned int readADC(void);
-void initLED();
+void initLED(void);
+void initADC(void);
 
 int main(void) {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	P6DIR &= ~0x01;  // set P6.0 as input
-	P6SEL |= 0x01;  // Set P6.0 as analog input
+	WDTCTL = WDTPW | WDTHOLD;	// Stop the watchdog timer
 
+	// Initialize the LEDs and ADC
 	initLED();
-
-	// call the configureADC function
-	configureADC();
+    initADC();
 
 	// Infinite loop
 	while(1) {
-	    unsigned int adcValue = readADC();  // Read in the ADC value
 
-	    if (adcValue > 3000) {
-	        P2OUT |= 0x04;  // Turn on P2.2
-	        P1OUT &= ~0x10;  // Turn off P1.4
-	        P1OUT |= 0x20;  // Turn on P1.5
-	        P2OUT &= ~0x10;  // Turn off P2.4
-	        P2OUT |= 0x20;  // Turn on P2.5
-	        P4OUT &= ~0x08;  // Turn off P4.3
+	    P2OUT |= 0x04;  // Turn on P2.2
+	    P1OUT &= ~0x10;  // Turn off P1.4
+	    P1OUT |= 0x20;  // Turn on P1.5
+	    P2OUT &= ~0x10;  // Turn off P2.4
+	    P2OUT |= 0x20;  // Turn on P2.5
+	    P4OUT &= ~0x08;  // Turn off P4.3
 
-	        // Flat/sharp LEDs
-	        P2OUT |= 0x04;  // Turn on P2.3
-	        P2OUT |= 0x40;  // Turn on P2.6
-	        P3OUT |= 0x80;  // Turn on P3.7
-	        P8OUT |= 0x02;  // Turn on P8.1
-	        P8OUT |= 0x04;  // Turn on P8.2
-	    }
-	    else {
-	        P2OUT &= ~0x04;  // Turn off P2.2
-	        P1OUT |= 0x10;  // Turn on P1.4
-	        P1OUT &= ~0x20;  // Turn off P1.5
-	        P2OUT |= 0x10;  // Turn on P2.4
-	        P2OUT &= ~0x20;  // Turn off P2.5
-	        P4OUT |= 0x08;  // Turn on P4.3
+	    // Flat/sharp LEDs
+	    P2OUT |= 0x04;  // Turn on P2.3
+	    P2OUT |= 0x40;  // Turn on P2.6
+	    P3OUT |= 0x80;  // Turn on P3.7
+	    P8OUT |= 0x02;  // Turn on P8.1
+	    P8OUT |= 0x04;  // Turn on P8.2
 
+	    _delay_cycles(1000000);
 
-	    }
+	    P2OUT &= ~0x04;  // Turn off P2.2
+	    P1OUT |= 0x10;  // Turn on P1.4
+	    P1OUT &= ~0x20;  // Turn off P1.5
+	    P2OUT |= 0x10;  // Turn on P2.4
+	    P2OUT &= ~0x20;  // Turn off P2.5
+	    P4OUT |= 0x08;  // Turn on P4.3
 	}
-
-//	return 0;
 }
 
 void initLED() {
@@ -74,16 +63,19 @@ void initLED() {
     //P8OUT &= 0x06;
 }
 
-//void configureADC(void) {
-    // Setup ADC12 for single-channel single-conversion mode
-  //  ADC12CTL0 = ADC12SHT0_2 | ADC12ON;             // Enable ADC, set sample time
-  //  ADC12CTL1 = ADC12CSTARTADD_0 | ADC12SHP;       // Use default start address, use sample and hold
-  //  ADC12MCTL0 = ADC12INCH_0;                      // Select input channel (P6.0)
-  //  ADC12CTL0 |= ADC12ENC;                         // Enable the ADC
-//}
+void initADC(void) {
+    // Disable the ADC to avoid conflicts during configuration
+    ADC12CTL0 = ADC12CTL0 & ~ADC12ENC;
 
-//unsigned int readADC(void) {
-  //  ADC12CTL0 |= ADC12SC;  // Start the conversion
-  //  while (ADC12CTL1 & ADC12BUSY);  // Wait for the conversion to finish
-  //  return ADC12MEM0;  // Return the value of the ADC result
-//}
+    ADC12CTL0 = ADC12SHT0_8 | ADC12ON;  // Set sampling time to 512 clocks and turn ADC on
+    ADC12CTL1 = ADC12CSTARTADD_0 | ADC12SHP;  // Single channel, single conversion, and SHP for internal clock
+    ADC12CTL2 = ADC12RES_2;  // 12-bit resolution
+
+    P6DIR &= ~0x01;  // set P6.0 as input
+    P6SEL |= 0x01;  // Set P6.0 as analog input
+
+//    ADC12IE0 = 0x01;  // Enable P6.0 interrupt
+
+    ADC12CTL0 |= ADC12ENC | ADC12SC;  // Turn on ADC12
+}
+
